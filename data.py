0919715342122991eaa2bc9ea0254733e79c8d13
Konrad Gawron <konrad.gawron@poczta.fm>
2020-12-26 11:02:42 +0100

    Add data.py file

---
+++ data.py
@@ -0,0 +1,34 @@
+import json
+
+
+class Videos:
+    def __init__(self):
+        try:
+            with open("videos.json", "r") as f:
+                self.videos = json.load(f)
+        except FileNotFoundError:
+            self.video = []
+
+    def all(self):
+        return self.videos
+
+    def get(self, id):
+        return self.videos[id]
+
+    def create(self, data):
+        data.pop('csrf_token')
+        self.videos.append(data)
+
+    def save_all(self):
+        with open("videos.json", "w") as f:
+            json.dump(self.videos, f)
+
+    def update(self, id, data):
+        data.pop('csrf_token')
+        self.videos[id] = data
+        self.save_all()
+
+
+
+
+videos = Videos()
