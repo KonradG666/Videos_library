7fe1f3ace3b255480bbf3b0adbb682465dea0f9e
Konrad Gawron <konrad.gawron@poczta.fm>
2020-12-29 18:20:36 +0100

    modify json and html files

--- forms.py
+++ forms.py
@@ -4,6 +4,7 @@ from wtforms.validators import DataRequired
 
 
 class MusicVideoLibrary(FlaskForm):
+    order_number =  StringField('Number', validators=[DataRequired()])
     title = StringField('Video Title', validators=[DataRequired()])
     band = StringField('Band Name', validators=[DataRequired()])
     genre = StringField('Music Kind', validators=[DataRequired()])
--- music_video_library.py
+++ music_video_library.py
@@ -10,7 +10,7 @@ def videos_list():
     form = MusicVideoLibrary()
     error = ""
     if request.method == "POST":
-        if form.validate_on_submit():
+        if form.validate_on_submit()
             videos.create(form.data)
             videos.save_all()
         return redirect(url_for("videos_list"))
@@ -20,15 +20,14 @@ def videos_list():
 
 @app.route("/library/<int:video_id>/", methods=["GET", "POST"])
 def video_add(video_id):
-    video = videos.add(video_id - 1)
+    video = videos.get(video_id - 1)
     form = MusicVideoLibrary(data=video)
-    error = ""
 
     if request.method == "POST":
         if form.validate_on_submit():
             videos.update(video_id - 1, form.data)
         return redirect(url_for("videos_list"))
-    return render_template("music.html", form=form, videos=videos.all(), error=error)
+    return render_template("music_id.html", form=form, video_id=video_id)
 
 @app.route("/index/<int:video_id>", methods=["PUT"])
 def video_update(video_id):
