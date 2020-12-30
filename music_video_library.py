89f6e293349950df0065afa3c64d4522b6d1ad3e
Konrad Gawron <konrad.gawron@poczta.fm>
2020-12-30 21:46:52 +0100

    Add updated writes

--- music_video_library.py
+++ music_video_library.py
@@ -17,6 +17,17 @@ def videos_list():
 
     return render_template("music.html", form=form, videos=videos.all(), error=error)
 
+@app.route("/library/<int:video_id>/", methods=["GET", "POST"])
+def video_update(video_id):
+    video = videos.get(video_id - 1)
+    form = MusicVideoLibrary(data=video)
+
+    if request.method == "POST":
+        if form.validate_on_submit():
+           videos.update(video_id - 1, form.data)
+        return redirect(url_for("videos_list"))
+    return render_template("music_id.html", form=form, video_id=video_id)
+
 
 @app.route("/library/<int:video_id>/", methods=["DELETE"])
 def delete_video(video_id):
@@ -25,10 +36,10 @@ def delete_video(video_id):
     video = videos.delete(video_id - 1)
 
 
-    return render_template("music_id.html", form=form, videos=videos.all(), error=error)
+    return render_template("music.html", form=form, video_id=video_id, error=error)
 
 @app.route("/library/<int:video_id>", methods=["PUT"])
-def video_update(video_id):
+def video_d(video_id):
     pass
 
 if __name__ == "__main__":
